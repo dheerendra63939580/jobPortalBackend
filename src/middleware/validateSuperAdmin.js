f7c9a064a8ac472e2313ccba5daff02f0e1b51dc
dheerendra kumar <testrpa85@gmail.com>
2025-10-26 18:45:27 +0530

    refactor: delete unecessary files.

--- src/middleware/validateSuperAdmin.js
+++
@@ -1,9 +0,0 @@
-import { User } from "../model/userModel.js";
-import { ErrorResponse } from "../utils/errorResponse.js";
-
-export const validateSuperAdmin = async (req, res, next) => {
-    const superAdmin = await User.findById(req?.userId);
-    if(!superAdmin || superAdmin?.role !== "superAdmin")
-        return next(new ErrorResponse("Unauthorized access", [], 400));
-    next();
-}
